e21548ab5dc16c6d1f8ccddfa32aa9ecac712018
saeid sadeh <saeid.sadeh@yahoo.com>
2016-06-21 21:48:48 +0000

    generating users controller and users/new view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 RottenMango::Application.routes.draw do
+  get "users/new"
+  get "users/create"
   resources :movies
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
