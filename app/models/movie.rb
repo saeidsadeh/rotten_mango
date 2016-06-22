90d597ef904c753dfb711ff6daf72a24b3fafafb
saeid sadeh <saeid.sadeh@yahoo.com>
2016-06-22 03:51:39 +0000

    routing root to movies

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,8 +1,9 @@
 class Movie < ActiveRecord::Base
+
   has_many :reviews
-  
+
   validates :title,
-  presence: true
+    presence: true
 
   validates :director,
     presence: true
@@ -19,13 +20,18 @@ class Movie < ActiveRecord::Base
   validates :release_date,
     presence: true
 
-  validate :release_date_is_in_the_past
+  validate :release_date_is_in_the_future
+
+  def review_average
+    reviews.sum(:rating_out_of_ten)/reviews.size
+  end
 
   protected
 
-  def release_date_is_in_the_past
+  def release_date_is_in_the_future
     if release_date.present?
-      errors.add(:release_date, "should be in the past") if release_date > Date.today
+      errors.add(:release_date, "should probably be in the future") if release_date < Date.today
     end
   end
+
 end
